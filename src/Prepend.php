235d67fd38fcd458dae41643851fb9e92f4ebd49
Jean-Christian Denis <contact@jcdenis.fr>
2025-09-09 17:30:13 +0200

    Use TraitProcess

--- src/Prepend.php
+++ src/Prepend.php
@@ -6,7 +6,7 @@ namespace Dotclear\Plugin\dcFilterDuplicate;
 
 use ArrayObject;
 use Dotclear\App;
-use Dotclear\Core\Process;
+use Dotclear\Helper\Process\TraitProcess;
 
 /**
  * @brief       dcFilterDuplicate prepend class.
@@ -15,8 +15,10 @@ use Dotclear\Core\Process;
  * @author      Jean-Christian Denis (author)
  * @copyright   GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-class Prepend extends Process
+class Prepend
 {
+    use TraitProcess;
+
     public static function init(): bool
     {
         return self::status(My::checkContext(My::PREPEND));
